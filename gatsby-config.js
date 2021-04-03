c407f2ff2ae0194d7cccf5e9141a9502a59ad63f
aznic95 <aznic95@gmail.com>
2021-04-03 17:50:59 +0900

    @update favi, theme_color

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,10 +32,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#003D8D`,
+        theme_color: `#003D8D`,
         display: `minimal-ui`,
-        icon: `src/images/favi.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
 
