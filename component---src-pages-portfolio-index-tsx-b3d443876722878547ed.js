20c79612ff4b3dbe28f611ba3b3ebb631b31bc2a
beardfriend <aznic95@gmail.com>
2021-07-02 13:58:14 +0900

    Updates

--- component---src-pages-portfolio-index-tsx-b3d443876722878547ed.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[967],{4729:function(e,t,a){"use strict";a.r(t);var l=a(7294);t.default=function(){return l.createElement("div",null,"hello world")}}}]);
-//# sourceMappingURL=component---src-pages-portfolio-index-tsx-b3d443876722878547ed.js.map
