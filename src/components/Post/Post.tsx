8debbde552eaf8c27b5301edb5b73dc3bf36f48b
beardfriend <beardfriend21@gmail.com>
2022-01-15 02:59:54 +0900

    infinite scroll version1.0

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,11 +1,61 @@
 import media from '@Globals/theme';
 import { useGlboalState } from '@Contexts/context';
+
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 import styled from 'styled-components';
 
-function PostUI({ node }) {
+const Spinner = styled.div<{ isLoading }>`
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+
+  &:after {
+    border-radius: 50%;
+    width: 10em;
+    height: 10em;
+  }
+  display: ${({ isLoading }) => (isLoading ? 'block' : 'none')};
+  margin: 0 auto;
+  font-size: 6px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(0, 0, 0, 0.2);
+  border-right: 1.1em solid rgba(0, 0, 0, 0.2);
+  border-bottom: 1.1em solid rgba(0, 0, 0, 0.2);
+  border-left: 1.1em solid #000000;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+
+  @-webkit-keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+function PostUI({ node, setRef }) {
   return (
     <Layout>
       <Link to={node.fields.slug}>
@@ -25,12 +75,19 @@ function PostUI({ node }) {
       </div>
 
       <p style={{ position: 'absolute', bottom: '2rem' }}>{node.frontmatter.date}</p>
+      <div ref={setRef} />
     </Layout>
   );
 }
 
 function Post() {
   const { NowCategory, NowTag } = useGlboalState();
+  const [count, setCount] = useState(3);
+  const [loading, setLoading] = useState(false);
+  const [ref, setRef] = useInfiniteScroll(() => {
+    loadMorePost();
+  });
+
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
@@ -60,25 +117,63 @@ function Post() {
       }
     }
   `);
-  const blogPost = data.allMarkdownRemark.edges;
 
+  const blogPost = data.allMarkdownRemark.edges;
+  async function loadMorePost() {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 600));
+    setCount((num) => {
+      if (num <= blogPost.length) return num + 2;
+      return num;
+    });
+    setLoading(false);
+  }
   if (NowCategory === 'All') {
     if (NowTag.length === 0) {
       return (
         <>
-          {blogPost.map((nodes: any) => {
+          {blogPost.slice(0, count).map((nodes: any) => {
             const { node } = nodes;
-            return <PostUI key={node.id} node={node} />;
+            return (
+              <>
+                <PostUI key={node.id} node={node} setRef={setRef} />
+              </>
+            );
           })}
+          {blogPost.length < count ? (
+            <div style={{ width: '100%', textAlign: 'center', margin: '2rem 0' }}>
+              <h1 style={{ fontSize: '2rem' }}>END</h1>
+            </div>
+          ) : (
+            <Spinner isLoading={loading} />
+          )}
         </>
       );
     }
     return (
       <>
-        {blogPost.map((nodes: any) => {
+        {blogPost.slice(0, count).map((nodes: any) => {
           const { node } = nodes;
           if (NowTag.filter((tag) => node.frontmatter.tags?.includes(tag)).length > 0) {
-            return <PostUI key={node.id} node={node} />;
+            return (
+              <>
+                {blogPost.slice(0, count).map((nodes: any) => {
+                  const { node } = nodes;
+                  return (
+                    <>
+                      <PostUI key={node.id} node={node} setRef={setRef} />
+                    </>
+                  );
+                })}
+                {blogPost.length < count ? (
+                  <div style={{ width: '100%', textAlign: 'center', margin: '2rem 0' }}>
+                    <h1 style={{ fontSize: '2rem' }}>END</h1>
+                  </div>
+                ) : (
+                  <Spinner isLoading={loading} />
+                )}
+              </>
+            );
           }
         })}
       </>
@@ -87,10 +182,28 @@ function Post() {
   if (NowTag.length === 0) {
     return (
       <>
-        {blogPost.map((nodes: any) => {
+        {blogPost.slice(0, count).map((nodes: any) => {
           const { node } = nodes;
           if (node.frontmatter.category === NowCategory) {
-            return <PostUI key={node.id} node={node} />;
+            return (
+              <>
+                {blogPost.slice(0, count).map((nodes: any) => {
+                  const { node } = nodes;
+                  return (
+                    <>
+                      <PostUI key={node.id} node={node} setRef={setRef} />
+                    </>
+                  );
+                })}
+                {blogPost.length < count ? (
+                  <div style={{ width: '100%', textAlign: 'center', margin: '2rem 0' }}>
+                    <h1 style={{ fontSize: '2rem' }}>END</h1>
+                  </div>
+                ) : (
+                  <Spinner isLoading={loading} />
+                )}
+              </>
+            );
           }
         })}
       </>
@@ -98,11 +211,29 @@ function Post() {
   }
   return (
     <>
-      {blogPost.map((nodes: any) => {
+      {blogPost.slice(0, count).map((nodes: any) => {
         const { node } = nodes;
         if (node.frontmatter.category === NowCategory) {
           if (NowTag.filter((tag) => node.frontmatter.tags?.includes(tag)).length > 0) {
-            return <PostUI key={node.id} node={node} />;
+            return (
+              <>
+                {blogPost.slice(0, count).map((nodes: any) => {
+                  const { node } = nodes;
+                  return (
+                    <>
+                      <PostUI key={node.id} node={node} setRef={setRef} />
+                    </>
+                  );
+                })}
+                {blogPost.length < count ? (
+                  <div style={{ width: '100%', textAlign: 'center', margin: '2rem 0' }}>
+                    <h1 style={{ fontSize: '2rem' }}>END</h1>
+                  </div>
+                ) : (
+                  <Spinner isLoading={loading} />
+                )}
+              </>
+            );
           }
         }
       })}
